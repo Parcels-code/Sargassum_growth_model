20e66a099ad6b652438bddc3bbfa5d7d1eff7fe1
Erik van Sebille <erikvansebille@gmail.com>
2026-04-21 13:08:40 +0200

    Only loading fieldset when output file doesn't exist

--- Manuscript_Figures/basin_simulation.py
+++ Manuscript_Figures/basin_simulation.py
@@ -21,51 +21,52 @@ release_lat = pts.latitude.where(pts, drop=True).values
 for type in ['Eulerian', 'Lagrangian']:
     for month in range(1, 13):
         startmonth = f"2024-{month:02d}"
-        fieldset = load_copernics_fieldset.create_fieldset(startmonth=startmonth)
-
-        #TODO these can be removed as fieldset constants
-        fieldset.add_constant('G', 9.81)  # Gravitational constant [m s-1]
-        # #Nitrogen half saturation constant
-        fieldset.add_constant('k_N', 0.001) #mmol/m3
-        #Overall maximal growth rate (Corbin & Oxenford)
-        fieldset.add_constant('MGR_SF3', 0.124)
-        fieldset.add_constant('MGR_SN1', 0.083)
-        fieldset.add_constant('MGR_SN8', 0.053)
-        #Set initial weight
-        fieldset.add_constant('initial_weight', 50) #grams
-
         filename = f"Simulation_Basin_{startmonth}_{type}.zarr"
 
-        pset = parcels.ParticleSet(
-            fieldset=fieldset,
-            pclass = SargassumParticle,
-            lon = release_lon,
-            lat = release_lat,
-            z = np.zeros_like(release_lon),
-            time = np.datetime64(f'{startmonth}-01T00:00:00'),
-        )
-
-        pfile = parcels.ParticleFile(
-            filename,
-            outputdt=np.timedelta64(2, 'h'),
-            chunks = (len(release_lon), 50),
-        )
-
-        if type == 'Eulerian':
-            kernels = [
-                sargassum_kernels.sargassum_biological_growth_model,
-            ]
-        else:
-            kernels = [
-                parcels.kernels.AdvectionRK4,
-                sargassum_kernels.di_Stokes_drift,
-                sargassum_kernels.windage_drift,
-                sargassum_kernels.sargassum_biological_growth_model,
-                sargassum_kernels.stranding,
-                sargassum_kernels.DeleteOutOfBounds,
-            ]
-
         if not os.path.exists(filename):
+            print(f"Running simulation for {startmonth} ({type})...")
+            fieldset = load_copernics_fieldset.create_fieldset(startmonth=startmonth)
+
+            #TODO these can be removed as fieldset constants
+            fieldset.add_constant('G', 9.81)  # Gravitational constant [m s-1]
+            # #Nitrogen half saturation constant
+            fieldset.add_constant('k_N', 0.001) #mmol/m3
+            #Overall maximal growth rate (Corbin & Oxenford)
+            fieldset.add_constant('MGR_SF3', 0.124)
+            fieldset.add_constant('MGR_SN1', 0.083)
+            fieldset.add_constant('MGR_SN8', 0.053)
+            #Set initial weight
+            fieldset.add_constant('initial_weight', 50) #grams
+
+            pset = parcels.ParticleSet(
+                fieldset=fieldset,
+                pclass = SargassumParticle,
+                lon = release_lon,
+                lat = release_lat,
+                z = np.zeros_like(release_lon),
+                time = np.datetime64(f'{startmonth}-01T00:00:00'),
+            )
+
+            pfile = parcels.ParticleFile(
+                filename,
+                outputdt=np.timedelta64(2, 'h'),
+                chunks = (len(release_lon), 50),
+            )
+
+            if type == 'Eulerian':
+                kernels = [
+                    sargassum_kernels.sargassum_biological_growth_model,
+                ]
+            else:
+                kernels = [
+                    parcels.kernels.AdvectionRK4,
+                    sargassum_kernels.di_Stokes_drift,
+                    sargassum_kernels.windage_drift,
+                    sargassum_kernels.sargassum_biological_growth_model,
+                    sargassum_kernels.stranding,
+                    sargassum_kernels.DeleteOutOfBounds,
+                ]
+
             pset.execute(
                 kernels,
                 runtime=np.timedelta64(31, 'D'),
